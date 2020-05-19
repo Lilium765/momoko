2a8f19189787b09e5f2fc34e49eb604580f7eafc
Lilium765 <65602343+Lilium765@users.noreply.github.com>
2020-05-19 23:37:36 +0900

    Add files via upload

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='*')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -10,12 +10,55 @@ token = os.environ['DISCORD_BOT_TOKEN']
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+    await ctx.send('お兄ちゃん、それはわからないよ。')
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+	
+
+class Roll(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.group(invoke_without_command=True)
+    async def roll(self,ctx):
+        embed = discord.Embed(color=0x0080ff)
+        embed.add_field(name='roll num a b n',value='a~bの範囲の値をn個表示するよ。',inline=False)
+        embed.add_field(name='roll key n 候補1 候補2 … 候補a', value='a個の候補の中からn個選ぶよ。',inline=False)
+        await ctx.send(embed=embed)
+
+    @roll.command()
+    async def num(self,ctx,a,b,n):
+        a = int(a)
+        b = int(b)
+        n = int(n)
+        embed = discord.Embed(color=0x0080ff)
+
+        if a>b:
+            a,b = b,a
+        if b - a < n:
+            embed.add_field(name='ERROR',value='個数が多すぎるよ、お兄ちゃん。')
+        else:
+            li = [i for i in range(a,b+1)]
+            res = random.sample(li,n)
+            embed.add_field(name="roll結果", value=sorted(res), inline=False)
+        await ctx.send(embed=embed)
+
+    @roll.command()
+    async def key(self,ctx,n,*key):
+        n = int(n)
+        embed = discord.Embed(color=0x0080ff)
+        if len(key)<n:
+            embed.add_field(name='ERROR',value='個数が多すぎるよ、お兄ちゃん。')
+        else:
+            res = ' '.join(random.sample(key,a))
+            embed.add_field(name="roll結果", value=res, inline=False)
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Roll(bot))
 
 
 bot.run(token)
